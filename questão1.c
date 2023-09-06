6bc47b08f632824580c3391dbcaad8ac1a56daee
CaiodeOliva <143459325+CaiodeOliva@users.noreply.github.com>
2023-09-05 21:07:17 -0300

    Create questão1.c

---
+++ questão1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int idade;
+
+
+// idade //
+printf("18:");
+scanf("%d", &idade);
+if(idade >= 18){
+printf("\nvoce e maior de idade\n\n");
+}
+else{
+printf("nvoce e menor de idade\n\n");
+}
+}
