1a928e7b6b9e19285a111fe26174e4e660af3fcb
CaiodeOliva <143459325+CaiodeOliva@users.noreply.github.com>
2023-09-05 21:09:41 -0300

    Create questão4.c

---
+++ questão4.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+int main(void) {
+    float peso;
+
+    // peso //
+    printf("digite seu peso:");
+    scanf("%f", &peso);
+    if(peso < 60.0){
+    printf("\oSeu peso:%.24", peso);
+    }
+}
